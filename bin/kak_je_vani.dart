41840512b35ae55f5519e4a1f95c7bbb7c1ad37f
Luka Lekcevic <luka.lekcevi@gmail.com>
2023-03-09 15:36:40 +0100

    Added option argument for city

--- bin/kak_je_vani.dart
+++ bin/kak_je_vani.dart
@@ -1,16 +1,28 @@
 import 'dart:io';
 import 'package:kak_je_vani/kak_je_vani.dart';
+import 'package:args/args.dart';
 
-void main(List<String> arg) async {
-  print("input city: ");
-  final city = stdin.readLineSync();
+void main(List<String> args) async {
+  final parser = ArgParser();
 
-  final w = await getWeather(city!);
-  print("Temperature: ${w[0]}°C | Feels like: ${w[1]}°C");
-  print("Weather: ${w[2]} | Wind speed : ${w[3]} m/s");
-  determineClothes(w);
+  parser.addOption("city", abbr: "c");
 
-  getClothes();
+  final results = parser.parse(args);
 
-  exit(1);
+  if (results.wasParsed("city")) {
+    print("=> ${results['city']}");
+
+    final city = results["city"];
+
+    final w = await getWeather(city!);
+    print("Temperature: ${w[0]}°C | Feels like: ${w[1]}°C");
+    print("Weather: ${w[2]} | Wind speed : ${w[3]} m/s");
+    determineClothes(w[1]);
+
+    getClothes();
+
+    exit(1);
+  } else {
+    print("no city");
+  }
 }
